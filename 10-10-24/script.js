b2de39d4edb9c81203c19cd513fcd00bc06c2d33
miriam-cabrera-alvarado <mcabreraa2200@alumno.ipn.mx>
2024-10-10 17:06:00 -0600

    k

---
+++ 10-10-24/script.js
@@ -0,0 +1,17 @@
+//comentario en una linea 
+/*
+comentario 
+multilineas
+*/
+
+//declaramos nuestra funcion
+
+function hello(){
+    return 'hola js'
+}
+
+console.log(hello());
+
+// investigar diferentes usos de funciones en js y ejemplificar cada una de ellas 
+
+// con uso de parametros, funcion dentro de otra funcion 
